e70f9b1f5ce15986c33bc1727969dedfda42fb17
seemakhan2000 <seemakhan36065@gmail.com>
2024-05-03 17:14:13 -0700

    jwtToken

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -6,8 +6,6 @@ const user = require('../../models/loginSchema.js')
 const jwt = require("jsonwebtoken");
 // Load environment variables from .env file
 require('dotenv').config();
-// Import the secretKey from config.js
-//const secretKey  = require('../config/config.js');
 
 
  exports.createData = async (req, resp) => {
